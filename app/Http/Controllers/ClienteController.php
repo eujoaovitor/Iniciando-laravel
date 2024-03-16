f26bf309932d11c230c50e4f302dd91c8928f2f9
eujoaovitor <joaovitorqxd@gmail.com>
2024-03-16 15:12:14 -0300

    Criando o banco e salvando nele

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ClienteResquest;
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    // LISTAGEM DE CLIENTES    
+    public function index()
+    {
+        return view("cliente/index");
+    }
+
+    // FORMULARIO DE CADASTRO DE CLIENTE
+    public function create()
+    {
+        return view("cliente/create");
+    }
+
+    // PAGINA MOSTRAR RESULTADO
+    public function mostrar()
+    {
+        return view("cliente/mostrar");
+    }
+
+    // SALVAR FORMULARIO NO BANCO
+    public function store(ClienteResquest $resquest)
+    {
+        //VALIDAR OS CAMPOS
+        $resquest->validated();
+
+        //SALVA NO BD
+        Cliente::create($resquest->all());
+
+        //REDIRECIONAMENTO
+        return redirect()->route('cliente.mostrar')->with('sucesso', 'Cliente cadastrado com sucesso!!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Jv\JvController;
+use App\Http\Controllers\ClienteController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,4 +8,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/clientes', [JvController::class, 'clientes']);
+// ROTA INDEX DO CLIENTE
+Route::get('/index-cliente', [ClienteController::class, 'index'])->name('cliente.index');
+
+//FORMULARIO DE CADASTRO
+Route::get('/create-cliente', [ClienteController::class, 'create'])->name('cliente.create');
+
+// MOSTRA RESULTADO
+Route::get('/mostrar-cliente', [ClienteController::class, 'mostrar'])->name('cliente.mostrar');
+
+// SALVA NO BANCO DE DADOS
+Route::post('/store-cliente', [ClienteController::class, 'store'])->name('cliente.store');
